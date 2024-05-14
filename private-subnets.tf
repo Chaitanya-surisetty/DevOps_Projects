b1e3c3845c73bcee54c2467a6a50889e135ae8b3
Chaitanya Surisetty <chaitanya.surisetty@gmail.com>
2024-05-14 18:07:51 +0100

    updated cidrs

--- private-subnets.tf
+++ private-subnets.tf
@@ -1,11 +1,14 @@
 resource "aws_subnet" "private-subnets" {
   #count             = 4 # 0 1 2
-  count             = length(local.new_private_subnet_cidrs)
+  #count             = length(local.new_private_subnet_cidrs)
   vpc_id            = aws_vpc.default.id
-  cidr_block        = element(local.new_private_subnet_cidrs, count.index)
-  availability_zone = element(var.azs, count.index)
+  #cidr_block        = element(local.new_private_subnet_cidrs, count.index)
+  #availability_zone = element(var.azs, count.index)
+  cidr_block        = local.new_private_subnet_cidrs
+  availability_zone = var.azs
   tags = {
-    Name              = "${var.vpc_name}-PrivateSubnet-${count.index + 1}"
+    #Name              = "${var.vpc_name}-PrivateSubnet-${count.index + 1}"
+    Name              = "${var.vpc_name}-PrivateSubnet"
     Terraform-Managed = "Yes"
     Env               = local.new_environment
     ProjectID         = local.projid
